9ce09550e775c1c5f098c7c77f865419fb3fd27c
Matt Bajorek <bajorek.matthew@gmail.com>
2016-09-17 19:32:37 -0400

    Added jawsdb credentials

--- connection.go
+++ connection.go
@@ -7,7 +7,7 @@ import (
 )
 
 func dbConnect() *sql.DB {
-	db, err := sql.Open("mysql", "root:@/burger_db")
+	db, err := sql.Open("mysql", "ilmd1mjdiyyftpuy:z5wjohny3dimm257@tcp(sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306)/tq3ji0sayjqfjwb7")
 	if err != nil {
 		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
 	}
