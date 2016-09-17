6ea64c66fe5573d43d25bdc5736a0c276dae3a5a
Matt Bajorek <bajorek.matthew@gmail.com>
2016-09-17 18:45:10 -0400

    Moved templates, removed fmt prints, and initialized vendor

--- router.go
+++ router.go
@@ -14,11 +14,8 @@ func Index(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	// Selects all burgers from db
 	burgers := selectAll()
 
-	// Prints out burgers
-	fmt.Println(burgers)
-
 	// Render to template
-	tpl, err := template.ParseFiles("public/assets/templates/index.gohtml")
+	tpl, err := template.ParseFiles("templates/index.gohtml")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -49,12 +46,8 @@ func Devour(res http.ResponseWriter, req *http.Request, params httprouter.Params
 	// Parse form data for value
 	devoured := req.FormValue("devoured")
 
-	fmt.Println(devoured)
-
 	// Grab id from params
 	id := params.ByName("id")
-
-	fmt.Println(id)
 
 	// Update burger with id to devoured
 	updateOne(devoured, id)
